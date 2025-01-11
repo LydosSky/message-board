79833b11195767c5c0383e68e6068e04113c07ec
Lydos <lydossky@gmail.com>
2025-01-11 15:08:08 +0300

    feat: now you can leave message to board

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(express.urlencoded({ extended: true }));
 app.use('/', indexRouter);
 
 app.listen(3000, function () {
