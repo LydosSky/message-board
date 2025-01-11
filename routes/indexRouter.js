98e88c61a0ad1c3942ee38680f375095a1b85ceb
Lydos <lydossky@gmail.com>
2025-01-11 15:15:11 +0300

    feat: now you can see details of a message

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,14 +1,17 @@
 const { Router } = require('express');
+const crypto = require('node:crypto');
 
 const indexRouter = Router();
 
 const messages = [
   {
+    id: 'a2e87d96-3c41-409b-b8ad-263c8b6d4650',
     text: 'Hi there!',
     user: 'Amando',
     added: new Date(),
   },
   {
+    id: 'a2e87d96-3c41-409b-b8ad-263c8b6d4650',
     text: 'Hello World!',
     user: 'Charles',
     added: new Date(),
@@ -16,7 +19,33 @@ const messages = [
 ];
 
 indexRouter.get('', function (req, res) {
-  res.render('index', { title: 'Mini Messageboard', messages });
+  res.render('index', {
+    title: 'Mini Messageboard',
+    messages,
+  });
+});
+
+indexRouter.get('/new', function (req, res) {
+  res.render('form');
+});
+
+indexRouter.get('/:id', function (req, res) {
+  const message = messages.reduce(
+    (prev, curr) => (curr.id === req.params.id ? curr : prev),
+    null,
+  );
+  res.render('detail', { message });
+});
+
+indexRouter.post('/new', function (req, res) {
+  const { messageText, messageUser } = req.body;
+  messages.push({
+    id: crypto.randomUUID(),
+    text: messageText,
+    user: messageUser,
+    added: new Date(),
+  });
+  res.redirect('/');
 });
 
 module.exports = indexRouter;
