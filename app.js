ad81a1813ac747f7640e74b79700e9dac4e44c76
Lydos <lydossky@gmail.com>
2025-01-14 22:12:31 +0300

    feat: nav site can be styled

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const indexRouter = require('./routes/indexRouter');
 
 const app = express();
 
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
+
 app.use('/', indexRouter);
 
 app.listen(3000, function () {
